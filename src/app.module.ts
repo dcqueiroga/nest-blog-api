3409347d3ca61fe92036840599a70496a0a0b544
Daniel Queiroga <daniel.queiroga@clevertech.biz>
2022-08-07 22:07:34 -0300

    Implementing User module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,11 @@ import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
